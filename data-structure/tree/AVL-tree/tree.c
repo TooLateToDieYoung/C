f431dce728fee083c27d01556bf6a86b9733480f
tolatetodieyoung1204 <tolatetodieyoung1204@gmail.com>
2023-01-12 15:02:00 +0800

    [update] 2023/1/12 15:00 - optimized AVL tree implements.

--- data-structure/tree/AVL-tree/tree.c
+++ data-structure/tree/AVL-tree/tree.c
@@ -15,117 +15,19 @@ struct tree_t {
   CompareFunction cmp;
 };
 
-/* Static Helper Function Begin ------------------------------------------------*/
-
-// * Tree_Constructor
-static node_t * _Node_Constructor(void * const item, node_t * const parent);
-
-// * Tree_Destructor
+// ? forward declaration
+static node_t * _Node_Constructor(void * const item);
 static void _Node_Destructor(node_t * const self);
-
-// * Tree_FarR
 static node_t * _Node_FarR(node_t * const self);
-
-// * Tree_FarL
 static node_t * _Node_FarL(node_t * const self);
-
-// * Balance
-static node_t * _Node_RotateL(node_t * const self);
-static node_t * _Node_RotateR(node_t * const self);
-static char     _Node_BF(node_t const * const self);
-static node_t * _Node_Balance(node_t * const self);
-
-// * Tree_Insert
 static node_t * _Node_Insert(node_t * const self, node_t * const add, CompareFunction cmp);
-
-// * Tree_Remove
 static node_t * _Node_Sink(node_t * const self);
 static node_t * _Node_Remove(node_t * const self, void * const item, CompareFunction cmp);
-
-// * Tree_isContain
 static bool_t _Node_isContain(node_t const * const self, void * const item, CompareFunction cmp);
-
-// * Tree_Height
 static size_t _Max(size_t a, size_t b);
 static size_t _Node_Height(node_t const * const self);
-
-// * Tree_Size
 static size_t _Node_Size(node_t const * const self);
 
-/* --------------------------------------------------- Static Helper Function End */
-
-/* Constructor */
-static node_t * _Node_Constructor(void * const item, node_t * const parent)
-{
-  node_t * const obj = (node_t*)calloc(1, sizeof(node_t));
-
-  if(!obj) return NULL;
-
-  obj->P    = parent;
-  obj->item = item;
-
-  return obj;
-}
-
-tree_t * Tree_Constructor(CompareFunction cmp)
-{
-  if(!cmp) return NULL;
-
-  tree_t * const obj = (tree_t*)calloc(1, sizeof(tree_t));
-
-  if(!obj) return NULL;
-
-  obj->cmp  = cmp;
-
-  return obj;
-}
-
-/* Destructor */
-static void _Node_Destructor(node_t * const self)
-{
-  if(!self) return;
-  
-  _Node_Destructor(self->L);
-  _Node_Destructor(self->R);
-
-  free(self);
-}
-
-task_t Tree_Destructor(tree_t * const self)
-{
-  if(!self) return Fail;
-
-  _Node_Destructor(self->root);
-
-  free(self);
-
-  return Success;
-}
-
-
-/* FarR */
-static node_t * _Node_FarR(node_t * const self)
-{
-  return self->R ? _Node_FarR(self->R) : self ;
-}
-
-void * Tree_FarR(tree_t const * const self)
-{
-  return Tree_isEmpty(self) ? NULL : _Node_FarR(self->root)->item ; 
-}
-
-/* FarL */
-static node_t * _Node_FarL(node_t * const self)
-{
-  return self->L ? _Node_FarL(self->L) : self ;
-}
-
-void * Tree_FarL(tree_t const * const self)
-{
-  return Tree_isEmpty(self) ? NULL : _Node_FarL(self->root)->item ; 
-}
-
-/* Balance*/
 static node_t * _Node_RotateL(node_t * const self)
 {
   // Take self->P as node a: [a->P, a->L, a->R] ~> [a->R, a->L, b->L]
@@ -163,7 +65,7 @@ static char _Node_BF(node_t const * const self)
   return _Node_Height(self->L) - _Node_Height(self->R);
 }
 
-static node_t * _Node_Balance(node_t * const self)
+static node_t * _Node_Adjust(node_t * const self)
 {
   if(!self) return NULL;
 
@@ -188,113 +90,166 @@ __Label:
   if(prev) {
     if(prev->L == self) prev->L = temp;
     else                prev->R = temp;
-    return _Node_Balance(temp->P);
+    return _Node_Adjust(temp->P);
   }
   
   return temp; // tree->root
 }
 
-/* Insert */
-static node_t * _Node_Insert(node_t * const self, node_t * const add, CompareFunction cmp)
+static node_t * _Node_Constructor(void * const item)
 {
-  switch (cmp(self->item, add->item)) {
+  node_t * const obj = (node_t*)calloc(1, sizeof(node_t));
 
-    case -1: {
-      if(self->L) return _Node_Insert(self->L, add, cmp);
-      self->L = add;
-      add->P = self;
-      return _Node_Balance(self);
-    }
+  if(!obj) return NULL;
 
-    case +1: {
-      if(self->R) return _Node_Insert(self->R, add, cmp);
-      self->R = add;
-      add->P = self;
-      return _Node_Balance(self);
-    }
-    
-    default: {
-      _Node_Destructor(add);
-      return NULL;
-    }
+  obj->item = item;
+  
+  return obj;
+}
+tree_t * Tree_Constructor(CompareFunction cmp)
+{
+  if(!cmp) return NULL;
 
-  }
+  tree_t * const obj = (tree_t*)calloc(1, sizeof(tree_t));
+
+  if(!obj) return NULL;
+
+  obj->cmp = cmp;
+
+  return obj;
 }
 
-task_t Tree_Insert(tree_t * const self, void * const item)
+static void _Node_Destructor(node_t * const self)
+{
+  if(!self) return;
+
+  _Node_Destructor(self->L);
+  _Node_Destructor(self->R);
+
+  free(self);
+}
+task_t Tree_Destructor(tree_t * const self)
 {
   if(!self) return Fail;
 
-  if(Tree_isContain(self, item)) return Fail;
+  _Node_Destructor(self->root);
 
-  node_t * const node = _Node_Constructor(item, NULL);
-
-  if(!node) return Fail;
-
-  self->root = Tree_isEmpty(self) ? node : _Node_Insert(self->root, node, self->cmp) ;
+  free(self);
 
   return Success;
 }
 
-/* Remove */
-static node_t * _Node_Sink(node_t * const self)
+static node_t * _Node_FarR(node_t * const self)
 {
-  if(!self->L && !self->R) {
-    node_t * const temp = self->P;
-    if(temp->L == self) self->P->L = NULL;
-    else                self->P->R = NULL;
-    _Node_Destructor(self);
-    return _Node_Balance(temp);
-  }
-
-  node_t * const far = self->L ? _Node_FarR(self->L) : _Node_FarL(self->R) ;
-
-  self->item = far->item;
-
-  return _Node_Sink(far);
+  return self->R ? _Node_FarR(self->R) : self ;
+}
+void * Tree_FarR(tree_t const * const self)
+{
+  return self && self->root ? _Node_FarR(self->root)->item : NULL ;
 }
 
+static node_t * _Node_FarL(node_t * const self)
+{
+  return self->L ? _Node_FarL(self->L) : self ;
+}
+void * Tree_FarL(tree_t const * const self)
+{
+  return self && self->root ? _Node_FarL(self->root)->item : NULL ;
+}
+
+static node_t * _Node_Insert(node_t * const self, node_t * const add, CompareFunction cmp)
+{
+  switch (cmp(self->item, add->item)) {
+    case -1: {
+      if(self->L) return _Node_Insert(self->L, add, cmp);
+      self->L = add;
+      add->P = self;
+      return _Node_Adjust(add);
+    }
+    case +1: {
+      if(self->R) return _Node_Insert(self->R, add, cmp);
+      self->R = add;
+      add->P = self;
+      return _Node_Adjust(add);
+    }
+    default: {
+      _Node_Destructor(add);
+      return _Node_Adjust(self); // ! Error case
+    }
+  }
+}
+task_t Tree_Insert(tree_t * const self, void * const item)
+{
+  if(!self) return Fail;
+
+  if(Tree_isContain(self, item)) return Success;
+
+  node_t * const node = _Node_Constructor(item);
+
+  if(!node) return Fail;
+
+  self->root = Tree_isEmpty(self) ? node : _Node_Insert(self->root, node, self->cmp);
+
+  return Success;
+}
+
+static node_t * _Node_Sink(node_t * const self)
+{
+  static node_t * temp = NULL;
+
+  temp = self;
+  if     (self->L) temp = _Node_FarR(self->L);
+  else if(self->R) temp = _Node_FarL(self->R);
+
+  if(self != temp) {
+    self->item = temp->item;
+    return _Node_Sink(temp);
+  }
+
+  temp = self->P;
+  if(temp->L == self) temp->L = NULL;
+  else                temp->R = NULL;
+  free(self);
+  return _Node_Adjust(temp);
+}
 static node_t * _Node_Remove(node_t * const self, void * const item, CompareFunction cmp)
 {
   switch (cmp(self->item, item)) {
     case -1: return _Node_Remove(self->L, item, cmp);
     case +1: return _Node_Remove(self->R, item, cmp);
-    case  0: return _Node_Sink(self);
-    default: return NULL;
+    default: return _Node_Sink(self);
   }
 }
-
 task_t Tree_Remove(tree_t * const self, void * const item)
 {
-  // ? Check if this tree contains item
-  if(!Tree_isContain(self, item)) return Fail;
+  if(!self) return Fail;
 
-  // ? Make sure that is not root
+  if(!Tree_isContain(self, item)) return Success;
+
+  // ? Check if the item is at this tree's root.
   if(self->cmp(self->root->item, item)) {
     self->root = _Node_Remove(self->root, item, self->cmp);
     return Success;
   }
   
-  // ? Make sure that root has leaves
+  // ? The item is at this tree's root but the root still has leaves.
   if(self->root->L || self->root->R) {
     self->root = _Node_Sink(self->root);
     return Success;
   }
-
-  // ? The last case: is the root and the root does not have any leaves
+  
+  // ? Last case: size of tree is 1 && root is exactly item. 
   _Node_Destructor(self->root);
   self->root = NULL;
 
   return Success;
 }
 
-/* isEmpty */
 bool_t Tree_isEmpty(tree_t const * const self)
 {
   return self ? self->root == NULL : True ;
 }
 
-/* isContain */
 static bool_t _Node_isContain(node_t const * const self, void * const item, CompareFunction cmp)
 {
   if(!self) return False;
@@ -306,34 +261,28 @@ static bool_t _Node_isContain(node_t const * const self, void * const item, Comp
     default: return False;
   }
 }
-
 bool_t Tree_isContain(tree_t const * const self, void * const item)
 {
-  return _Node_isContain(self->root, item, self->cmp);
+  return self ? _Node_isContain(self->root, item, self->cmp) : False ;
 }
 
-/* Height */
 static size_t _Max(size_t a, size_t b)
 {
   return a > b ? a : b ;
 }
-
 static size_t _Node_Height(node_t const * const self)
 {
   return self ? _Max(_Node_Height(self->L), _Node_Height(self->R)) + 1 : 0 ;
 }
-
 size_t Tree_Height(tree_t const * const self)
 {
   return self ? _Node_Height(self->root) : 0 ;
 }
 
-/* Size */
 static size_t _Node_Size(node_t const * const self)
 {
-  return self ? _Node_Size(self->L) + _Node_Size(self->R) + 1 : 0 ; 
+  return self ? _Node_Size(self->L) + _Node_Size(self->R) + 1 : 0 ;
 }
-
 size_t Tree_Size(tree_t const * const self)
 {
   return self ? _Node_Size(self->root) : 0 ;
