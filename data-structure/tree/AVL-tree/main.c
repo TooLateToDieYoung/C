6e8c6870367ceeea257fdbbdad674c88216e0aa3
tolatetodieyoung1204 <tolatetodieyoung1204@gmail.com>
2023-01-12 12:40:33 +0800

    [update] 2023/1/11 14:01 - AVL tree test code updated.

--- data-structure/tree/AVL-tree/main.c
+++ data-structure/tree/AVL-tree/main.c
@@ -12,20 +12,18 @@ static char compare(void const * const a, void const * const b)
 
 int main(void)
 {
-  char data[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  char data[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // "WEFGA672KCD4J08RZSTUHI1LMO53PQ9NVXYB";
   const size_t len = strlen(data);
 
   tree_t * const tree = Tree_Constructor(compare);
 
-  for(size_t i = 0; i < len; ++i) Tree_Insert(tree, data + i);
+  for(size_t i = 0; i < len; ++i) Tree_Insert(tree, &data[i]);
 
   printf("Before ->  Height: %2ld, Size: %2ld\r\n", Tree_Height(tree), Tree_Size(tree));
   
-  char * c = NULL;
   for(size_t i = 0; !Tree_isEmpty(tree); ++i) {
-    c = (char*)Tree_FarR(tree);
-    Tree_Remove(tree, c);
-    printf("Remove: %c, Height: %2ld, Size: %2ld\r\n", *c, Tree_Height(tree), Tree_Size(tree));
+    Tree_Remove(tree, &data[i]);
+    printf("Remove: %c, Height: %2ld, Size: %2ld\r\n", data[i], Tree_Height(tree), Tree_Size(tree));
   }
 
   return Tree_Destructor(tree);
