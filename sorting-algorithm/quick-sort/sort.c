664a37abadc094d73e42f74cc6a5bd6ef73d68e4
tolatetodieyoung1204 <tolatetodieyoung1204@gmail.com>
2023-01-16 10:26:45 +0800

    [update] 2023/1/16 10:26 - quick sort done

--- sorting-algorithm/quick-sort/sort.c
+++ sorting-algorithm/quick-sort/sort.c
@@ -1,7 +1,32 @@
 #include "sort.h"
 
+static void _Swap(char * const a, char * const b)
+{
+  char t = *a;
+  *a = *b;
+  *b =  t;
+}
+
 char * Sort(char array[], size_t len, CompareFunction cmp)
 {
-  // TODO
+  if(len < 2) return array;
+
+  const char index = array[0];
+
+  size_t c = 1;
+  size_t h = 0;
+  size_t t = len - 1;
+
+  while( h < t ) {
+    switch (cmp(index, array[c])) {
+      case -1: _Swap(&array[t--], &array[c]);   continue;
+      case +1: _Swap(&array[h++], &array[c++]); continue;
+      default: continue;
+    }
+  }
+
+  Sort(&array[0],       c, cmp);
+  Sort(&array[c], len - c, cmp);
+
   return array;
 }
