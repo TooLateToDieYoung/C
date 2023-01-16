56eb5000e3ced6b5303963a5e1ac91d258f0facd
tolatetodieyoung1204 <tolatetodieyoung1204@gmail.com>
2023-01-16 14:35:30 +0800

    [update] 2023/1/16 14:22 - shell sort done

--- sorting-algorithm/shell-sort/sort.c
+++ sorting-algorithm/shell-sort/sort.c
@@ -2,6 +2,19 @@
 
 char * Sort(char array[], size_t len, CompareFunction cmp)
 {
-  // TODO
+  for(size_t gap = len >> 1; gap; gap >>= 1)
+  {
+    for(size_t shell = gap; shell < len; ++shell)
+    {
+      size_t curr = shell;
+      char temp = array[curr];
+      for(;
+        curr >= gap && cmp(temp, array[curr - gap]) == -1; 
+        curr -= gap
+      ) array[curr] = array[curr - gap];
+      array[curr] = temp;
+    }
+  }
+
   return array;
 }
