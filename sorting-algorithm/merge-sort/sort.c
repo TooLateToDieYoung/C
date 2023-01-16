627490d37174d19cf60a2cbc9d8c95ebd0810429
tolatetodieyoung1204 <tolatetodieyoung1204@gmail.com>
2023-01-16 13:32:00 +0800

    [update] 2023/1/16 13:31 - merge sort done

--- sorting-algorithm/merge-sort/sort.c
+++ sorting-algorithm/merge-sort/sort.c
@@ -1,7 +1,37 @@
 #include "sort.h"
+#include <stdlib.h>
+
+static char * _MergeSort(char array[], size_t head, size_t tail, CompareFunction cmp)
+{
+  if( !(head + 1 < tail) ) return array;
+
+  const size_t half = ( head + tail ) >> 1 ;
+
+  _MergeSort(array, head, half, cmp);
+  _MergeSort(array, half, tail, cmp);
+
+  size_t i = head;
+  size_t j = half;
+  const size_t len = tail - head;
+
+  char * const merge = (char*)calloc(len, sizeof(char));
+
+  size_t curr = 0;
+
+  while( i < half && j < tail )
+    merge[curr++] = cmp(array[i], array[j]) == -1 ? array[i++] : array[j++] ;
+
+  if     ( !(i < half) ) while( j < tail ) merge[curr++] = array[j++];
+  else if( !(j < tail) ) while( i < half ) merge[curr++] = array[i++];
+
+  for(curr = 0; curr < len; ++curr) array[head + curr] = merge[curr];
+
+  free(merge);
+
+  return array;
+}
 
 char * Sort(char array[], size_t len, CompareFunction cmp)
 {
-  // TODO
-  return array;
+  return _MergeSort(array, 0, len, cmp);
 }
